4cc42e16b498eb665fe9082c98d5d2d4f1be4e1f
nicoleyip1876 <17805096+nicoleyip1876@users.noreply.github.com>
2020-05-05 02:41:20 +0000

    Updating all questions

--- fix-code-1/main.cpp
+++ fix-code-1/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 struct Hero {
-  string name,
-  string title,
-  unsigned int age,
-  string weapon
+  string name;
+  string title;
+  unsigned int age;
+  string weapon;
 };
 
 int main() {
@@ -17,7 +17,7 @@ int main() {
   cin >> my_hero.title;
   cout << "What is your hero's age: ";
   cin >> my_hero.age;
-  cout << "What is your hero's weapon of choice: "
+  cout << "What is your hero's weapon of choice: ";
   cin >> my_hero.weapon;
   return 0;
 }
